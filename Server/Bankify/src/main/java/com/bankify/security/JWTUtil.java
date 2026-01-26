85b77166057dd35452c7d6130790ce2acd34975d
ritikubey <ritikubey84@gmail.com>
2026-01-26 18:17:08 +0530

    JWT Config and Filter added

--- Server/Bankify/src/main/java/com/bankify/security/JWTUtil.java
+++ Server/Bankify/src/main/java/com/bankify/security/JWTUtil.java
@@ -2,14 +2,23 @@ package com.bankify.security;
 
 import java.security.Key;
 import java.util.Date;
+import java.util.List;
+import java.util.stream.Collectors;
+
+import javax.crypto.SecretKey;
 
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.AuthorityUtils;
 import org.springframework.stereotype.Component;
 
 import com.bankify.entities.User;
 
 import jakarta.annotation.PostConstruct;
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.JwtParser;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.security.Keys;
 
@@ -23,7 +32,9 @@ public class JWTUtil {
 	@Value(value = "jwt.token.secret")
 	private String jwtSecret;
 	
-	private Key jwtKey;
+//	private Key jwtKey;
+	
+	private SecretKey jwtKey;
 	
 	@PostConstruct
 	public void init() {
@@ -36,15 +47,38 @@ public class JWTUtil {
 		
 		String subject=""+user.getId();
 		
-		String roles=user.getAuthorities.stream().
+		String roles=user.getAuthorities().stream().
 				map(authority->authority.getAuthority()).
 				collect(Collectors.joining(","));
 		
-		String token=Jwts.builder().
-				setSubject(subject).
-				setIssuedAt(new Date()).
-				setExpiration(new Date(System.currentTimeMillis()+jwtExpiration)).
-				claim("roles", roles)
+		String token=Jwts.builder()
+                .subject(subject)
+                .issuedAt(new Date())
+                .expiration(new Date(System.currentTimeMillis() + jwtExpiration))
+                .claim("role", roles)
+                .signWith(jwtKey)   
+                .compact();
+		
+		return token;
+	}
+	
+	public Authentication validateToken(String token) {
+		
+		JwtParser parser=Jwts.parser().verifyWith(jwtKey).build();
+		
+		Claims claims=parser
+                .parseSignedClaims(token)   
+                .getPayload();
+		
+		String userId=claims.getSubject();
+		
+		String roles=(String) claims.get("role");
+		
+		List<GrantedAuthority> authorities=AuthorityUtils.commaSeparatedStringToAuthorityList(roles);
+		
+		Authentication auth=new UsernamePasswordAuthenticationToken(userId,null, authorities);
+		
+		return auth;
 		
 	}
 	
